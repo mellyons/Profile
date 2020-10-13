ad325544d66e3d4344a93de23682541bdfaa6341
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-13 16:03:27 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -1,3 +1,11 @@
+function validateForm() {
+  var x = document.forms["myForm"]["wholeName"]["Address"]["Location"]["Zip"]["Phone"]["Email"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
 function msg() {
   alert("User Name and Information would be displayed here")
 }
