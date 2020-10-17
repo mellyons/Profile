22f38906e157720ca91f2a065eb5a9debc1e4c04
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-16 19:03:18 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -1,8 +1,12 @@
-function msg() {
+function msgSend() {
   var x = document.forms["myForm"]["wholeName"]["Address"]["Location"]["Zip"]["Phone"]["Email"].value;
   if (x == "") {
     alert("Name must be filled out");
     return false;
   }
+}
+
+function msgReset() {
+  
 }
 
