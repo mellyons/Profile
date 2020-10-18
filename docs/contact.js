02d0bf2bdcb322aed1d36b7bdd29b73adfc6ee1d
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-18 14:24:41 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -1,12 +1,37 @@
-function msgSend() {
-  var x = document.forms["myForm"]["wholeName"]["Address"]["Location"]["Zip"]["Phone"]["Email"].value;
-  if (x == "") {
-    alert("Name must be filled out");
+function validateForm() {
+  var name = document.forms["myForm"]["wholeName"];
+  var address = document.forms["myForm"]["Address"];
+  var location = document.forms["myForm"]["Location"];
+  var zip = document.forms["myForm"]["Zip"];
+  var phone = document.forms["myForm"]["Phone"];
+  var email = document.forms["myForm"]["Email"];
+  
+  
+  if (name == "") {
+    alert("Please enter first and last name");
     return false;
   }
+  
+  if (address == "") {
+    alert("Please enter mailing address");
+    return false;
+  }
+  if (location == "") {
+    alert("Please enter city and state");
+    return false;
+  }
+  if (zip == "") {
+    alert("Please enter zip code");
+    return false;
+  }
+  if (phone == "") {
+    alert("Please enter phone number");
+    return false;
+  }
+  if (email == "") {
+    alert("Please enter email address");
+    return false;
+  }  
 }
 
-function msgReset() {
-  
-}
 
