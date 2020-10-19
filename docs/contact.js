be1a3c169239c21635c9bdd3575696b380179daf
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-19 16:16:12 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -1,40 +1,31 @@
 function validateForm() {
-  var name = document.forms["myForm"]["wholeName"].value;
-  var address = document.forms["myForm"]["Address"].value;
-  var location = document.forms["myForm"]["Location"].value;
-  var zip = document.forms["myForm"]["Zip"].value;
-  var phone = document.forms["myForm"]["Phone"].value;
-  var email = document.forms["myForm"]["Email"].value;
+  var name = document.forms["myForm"]["wholeName"];
+  var address = document.forms["myForm"]["Address"];
+  var location = document.forms["myForm"]["Location"];
+  var zip = document.forms["myForm"]["Zip"];
+  var phone = document.forms["myForm"]["Phone"];
+  var email = document.forms["myForm"]["Email"];
   
   
-  if (name.length<1) {
-    document.getElementById("err1")"Please enter first and last name *"
+  if (name.value == "") {
+    window.alert("Please enter first and last name");
   }
-  if (address.length<1) {
-    document.getElementById("err2") = "Please enter first and last name *";
+  if (address.value == "") {
+    window.alert("Please enter your address");
   }
-  if (location.length<1) {
-    document.getElementById("err3") = "Please enter first and last name *";
+  if (location.value == "") {
+    window.alert("Please enter your city and state");
   }
-  if (zip.length<1) {
-   document.getElementById("err4") = "Please enter first and last name *";
+  if (zip.value == "") {
+   window.alert("Please enter your zip code");
   }
-  if (phone.length<1) {
-    document.getElementById("err5") = "Please enter first and last name *";
+  if (phone.value == "") {
+    window.alert("Please enter your phone number");
   }
-  if (email.length<1) {
-   document.getElementById("err6") = "Please enter first and last name *";
+  if (email.value == "") {
+   window.alert("Please enter a valid email address");
   }
-  if(name.length<1 || address.length<1 || location.length<1 || zip.length<1 || phone.length<1 || email.length<1) {
-  return false;
-  }
+  
+  return true;
 }
 
-
-function msgSend() {
-  alert("Your information has been sent")
-}
-
-function msgReset() {
-  alert("Put code that will take out user information")
-}
