cc007e9f1772f07f1179b5a3cb2e4d09e8de9518
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-19 17:11:17 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -9,21 +9,33 @@ function validateForm() {
   
   if (name.value == "") {
     window.alert("Please enter first and last name");
+    name.focus();
+    return false;
   }
   if (address.value == "") {
     window.alert("Please enter your address");
+    address.focus();
+    return false;
   }
   if (location.value == "") {
     window.alert("Please enter your city and state");
+    location.focus();
+    return false;
   }
   if (zip.value == "") {
    window.alert("Please enter your zip code");
+   zip.focus();
+   return false;
   }
   if (phone.value == "") {
     window.alert("Please enter your phone number");
+    phone.focus();
+    return false;
   }
   if (email.value == "") {
-   window.alert("Please enter a valid email address");
+    window.alert("Please enter a valid email address");
+    email.focus();
+    return false;
   }
   
   return true;
