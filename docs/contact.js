803e3a1eaef2c8ddb3f0cda6a561239891d47b95
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-19 13:13:48 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -30,4 +30,10 @@ function validateForm() {
   }
 }
 
+function msgSend() {
+  alert("Your information has been sent");
+}
 
+function msgReset() {
+  alert("Put code that will take out user information");
+}
