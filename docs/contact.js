00e6d526ba5336edbbaf48d985af2e7626d40638
mellyons <39783057+mellyons@users.noreply.github.com>
2020-10-19 15:01:20 -0500

    Update contact.js

--- docs/contact.js
+++ docs/contact.js
@@ -8,22 +8,22 @@ function validateForm() {
   
   
   if (name.length<1) {
-    document.getElementById("err1")"Please enter first and last name";
+    document.getElementById("err1")"Please enter first and last name *"
   }
   if (address.length<1) {
-    document.getElementById("err2") = "Please enter first and last name";
+    document.getElementById("err2") = "Please enter first and last name *";
   }
   if (location.length<1) {
-    document.getElementById("err3") = "Please enter first and last name";
+    document.getElementById("err3") = "Please enter first and last name *";
   }
   if (zip.length<1) {
-   document.getElementById("err4") = "Please enter first and last name";
+   document.getElementById("err4") = "Please enter first and last name *";
   }
   if (phone.length<1) {
-    document.getElementById("err5") = "Please enter first and last name";
+    document.getElementById("err5") = "Please enter first and last name *";
   }
   if (email.length<1) {
-   document.getElementById("err6") = "Please enter first and last name";
+   document.getElementById("err6") = "Please enter first and last name *";
   }
   if(name.length<1 || address.length<1 || location.length<1 || zip.length<1 || phone.length<1 || email.length<1) {
   return false;
